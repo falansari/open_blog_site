427f558b052544adbd4c9892c2f33211a35d2a55
Fatima A. Alansari <fatima.a.alansari@outlook.com>
2018-04-25 21:08:39 +0300

    articles view page template done closes #10

--- public_html/articles/view.php
+++ public_html/articles/view.php
@@ -11,7 +11,7 @@
      * @link     https://github.com/fatima-alansari/open_blog_site
      */
       
-      require '../layouts/head.html';
+      require_once $_SERVER['DOCUMENT_ROOT'].'/layouts/head.html';
     ?>
 
   <body class='container'>
@@ -21,38 +21,53 @@
 
       $articleId = $_GET['id'];
 
-      require_once '../../Database.php';
-      $db = Database::getInstance();
+    if ($articleId) {
+        include_once $_SERVER['DOCUMENT_ROOT'].'/../Database.php';
+        $db = Database::getInstance();
 
-      /**
-       * Update article's view count
-       */
-      $updateCountQuery = 'CALL proc_update_article_view_count('.$articleId.')';
-      $db->querySQL($updateCountQuery);
+        /**
+         * Update article's view count
+         */
+        $updateCountQuery = 'CALL proc_update_article_view_count('.$articleId.')';
+        $db->querySQL($updateCountQuery);
 
-      /**
-       * Fetch article's data
-       */
-      $fetchQuery = 'CALL proc_fetch_article_info('.$articleId.')';
-      $result = $db->singleFetch($fetchQuery);
+        /**
+         * Fetch article's data
+         */
+        $fetchQuery = 'CALL proc_fetch_article_info('.$articleId.')';
+        $result = $db->singleFetch($fetchQuery);
 
-    if ($result) {
-      print('title: '.$result->title);
+        if (!$result) {
+            echo 'This article does not exist.';
+        }
     } else {
-        echo 'This article does not exist.';
+        echo 'Invalid url.';
     }
     ?>
 
     <main class="text-center">
       <div class="navbar-template text-center">
-        <h1 class="text-capitalize"><?php /* article title */ ?></h1>
-        <p class="lead text-info"><?php /* article subtitle */ ?></p>
+        <h1 class=""><?php echo $result->title ?></h1>
+        <h2 class="lead text-info"><?php echo $result->subtitle ?></h2>
+        <h4>By 
+            <?php
+                echo '<a class="link" href="/users/articles.php?id='
+                  .$result->user_id.'">'
+                  .$result->first_name.' '.$result->last_name.'</a>'; 
+            ?>
+        </h4>
+        <h5 class="fa fa-clock-o">
+            <?php echo(date('M jS Y\, h:i:s a', strtotime($result->created_at))); ?>
+        </h5>
+        <br />
+        
+        <p class="lead text-justify"><?php echo $result->content ?></p>
       </div>
     </main>
 
   </body>
 
     <?php
-      require '../layouts/footer.html';
+      require_once $_SERVER['DOCUMENT_ROOT'].'/layouts/footer.html';
     ?>
 </html>
