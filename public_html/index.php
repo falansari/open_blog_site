7bf22d846668c3fb061485c2319f44298a1707eb
Fatima A. Alansari <fatima.a.alansari@outlook.com>
2018-04-25 14:47:19 +0300

    Close #4 #28 & progress on #14 (#29)
    * top articles & views updater SQL #4 #14
    
    * Database class added & phpdotenv removed closes #28
    
    * SQL script for top articles & views counter #4 #14
    
    * fixed directory structure to remove pages from url
    
    * Show top five articles on homepage closes #4

--- public_html/index.php
+++ public_html/index.php
@@ -23,8 +23,50 @@
     <main>
       <div class="navbar-template text-center">
         <h1>Top Articles</h1>
-        <p class="lead text-info">What are you looking for?</p>
+        <p class="lead text-info">From the world's top bloggers!</p>
       </div>
+
+        <?php
+          require_once '../Database.php';
+
+          $db = Database::getInstance();
+
+          $query = 'CALL top_five_articles()';
+
+          $result = $db->multiFetch($query);
+
+        if ($result) {
+            ?>
+
+            <div class="container">
+            <table class="table-hover table-responsive table-condensed">
+              <tr>
+                <td>Title</td>
+                <td>Subtitle</td>
+                <td>Date Posted</td>
+                <td>Times Viewed</td>
+                <td>Creator</td>
+              </tr>
+            <?php
+
+            for ($i = 0; $i < count($result); $i++ ) {
+                echo '<tr>
+                <td class="text-capitalize">
+                  <a href="/articles/'.$result[$i]->article_id.'">'
+                  .$result[$i]->title .'</a></td>
+                <td>'.$result[$i]->subtitle.'</td>
+                <td>'.date('M jS Y', strtotime($result[$i]->created_at)).'</td>
+                <td>'.$result[$i]->view_count.'</td>
+                <td><a href="/users/'.$result[$i]->user_id.'">'
+                  .$result[$i]->username.'</a></td>
+                </tr>';
+            }
+            echo '</table>';
+        } else {
+            echo 'no results found.';
+        }
+        ?>
+        </div> <!-- END TABLE -->
     </main>
 
   </body>
