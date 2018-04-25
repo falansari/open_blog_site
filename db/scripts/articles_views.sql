d71328c6df0c576e382f77445458c70dda287507
Fatima A. Alansari <fatima.a.alansari@outlook.com>
2018-04-25 14:42:09 +0300

    SQL script for top articles & views counter #4 #14

--- db/scripts/articles_views.sql
+++ db/scripts/articles_views.sql
@@ -1,14 +1,16 @@
 /**
-Procedure: returns top five most popular articles based on view count.
-Called in PHP article search script.
+Procedure: returns top five most popular articles based on view count, 
+even if fk user_id is null.
  */
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS top_five_articles$$
 
 CREATE PROCEDURE top_five_articles()
-  SELECT ua.id, ua.title, ua.subtitle, ua.view_count, ua.created_at, ua.user_id
+  SELECT ua.id article_id, ua.title, ua.subtitle, ua.view_count, ua.created_at, ua.user_id, u.username
   FROM user_articles ua
+  LEFT JOIN users u
+  ON ua.user_id = u.id
   ORDER BY ua.view_count DESC
   LIMIT 5 $$
 $$
